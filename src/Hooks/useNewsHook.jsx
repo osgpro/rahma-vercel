ec2b98d4145bbb4c54289e2a27eb1d1c4e689d58
Development-osgpro <aalshami665@gmail.com>
2022-08-15 15:28:04 +0300

    submit

--- src/Hooks/useNewsHook.jsx
+++ src/Hooks/useNewsHook.jsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const useNewsHook = (query, pageNumber) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [news, setNews] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
 
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
-    const [news, setNews] = useState([])
-    const [hasMore, setHasMore] = useState(false)
+  useEffect(() => {
+    setNews([]);
+  }, [query]);
 
-    useEffect(() => {
-        setNews([])
-    }, [query])
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "GET",
+      url: "https://rahmaww.org/api/news",
+      params: { q: query, page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setNews((prevNews) => {
+          return [...prevNews, ...res.data["news"].map((b) => b)];
+        });
+        setHasMore(
+          res.data["news"].length > 0 && res.data["pages"] > pageNumber
+        );
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [query, pageNumber]);
 
-    useEffect(() => {
-        setLoading(true)
-        setError(false)
-        let cancel
-        axios({
-            method: 'GET',
-            url: '/api/news',
-            params: { q: query, page: pageNumber },
-            cancelToken: new axios.CancelToken(c => cancel = c)
-        }).then(res => {
-            setNews(prevNews => {
-                return [...prevNews, ...res.data['news'].map(b => b)]
-            })
-            setHasMore(res.data['news'].length > 0 && res.data['pages'] > pageNumber)
-            setLoading(false)
-        }).catch(e => {
-            if (axios.isCancel(e)) return
-            setError(true)
-        })
-        return () => cancel()
-    }, [query, pageNumber])
+  return { loading, error, news, hasMore };
+};
 
-    return { loading, error, news, hasMore }
-}
-
-export default useNewsHook
+export default useNewsHook;
--- src/Hooks/useStoriesHook.jsx
+++ src/Hooks/useStoriesHook.jsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const useStoriesHook = (pageNumber) => {
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [stories, setStories] = useState([]);
+  const [hasMore, setHasMore] = useState(false);
 
-    const [loading, setLoading] = useState(true)
-    const [error, setError] = useState(false)
-    const [stories, setStories] = useState([])
-    const [hasMore, setHasMore] = useState(false)
+  useEffect(() => {
+    setStories([]);
+  }, []);
 
-    useEffect(() => {
-        setStories([])
-    }, [])
+  useEffect(() => {
+    setLoading(true);
+    setError(false);
+    let cancel;
+    axios({
+      method: "GET",
+      url: "https://rahmaww.org/api/success-stories/",
+      params: { page: pageNumber },
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        setStories((prevNews) => {
+          return [...prevNews, ...res.data["stories"].map((b) => b)];
+        });
+        setHasMore(
+          res.data["stories"].length > 0 && res.data["pages"] > pageNumber
+        );
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+        setError(true);
+      });
+    return () => cancel();
+  }, [pageNumber]);
 
-    useEffect(() => {
-        setLoading(true)
-        setError(false)
-        let cancel
-        axios({
-            method: 'GET',
-            url: '/api/success-stories/',
-            params: { page: pageNumber },
-            cancelToken: new axios.CancelToken(c => cancel = c)
-        }).then(res => {
-            setStories(prevNews => {
-                return [...prevNews, ...res.data['stories'].map(b => b)]
-            })
-            setHasMore(res.data['stories'].length > 0 && res.data['pages'] > pageNumber)
-            setLoading(false)
-        }).catch(e => {
-            if (axios.isCancel(e)) return
-            setError(true)
-        })
-        return () => cancel()
-    }, [pageNumber])
+  return { loading, error, stories, hasMore };
+};
 
-    return { loading, error, stories, hasMore }
-}
-
-export default useStoriesHook
+export default useStoriesHook;
